d9fe44b64a1a7253ae2f3eacc96c1844b99fcd24
Lilanka Ravinath <lilankaravinath@outlook.com>
2017-06-13 11:00:51 +0530

    commented changin DOM elements

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ console.log("Child nodes: ", mainTitle.childNodes.length);
 var myLinks = document.getElementsByTagName("a");
 console.log("Links: ", myLinks.length);
 
-var mainContent = document.getElementById("mainContent");
-mainContent.setAttribute("align","right");
+//var mainContent = document.getElementById("mainContent");
+//mainContent.setAttribute("align","right");
 
-var sidebar = document.getElementById("sidebar");
-console.log(sidebar.innerHTML);
+//var sidebar = document.getElementById("sidebar");
+//console.log(sidebar.innerHTML);
