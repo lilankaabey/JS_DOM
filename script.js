f34eeebef47d037f8d2b500af9b8cba3e5c4d05a
Lilanka Ravinath <lilankaravinath@outlook.com>
2017-06-13 10:06:28 +0530

    Checking how work nodes

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
+var mainTitle = document.getElementById("mainTitle");
 
-
+console.log("This is an element of type: ", mainTitle.nodeType);
+console.log("the Inner HTML is ", mainTitle.innerHTML );
+console.log("Child nodes: ", mainTitle.childNodes.length);
