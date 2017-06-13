cba9117896879a12ae9c14b099e67b7b1ec05ed5
Lilanka Ravinath <lilankaravinath@outlook.com>
2017-06-13 10:27:57 +0530

    Checking How work TagName

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ var mainTitle = document.getElementById("mainTitle");
 console.log("This is an element of type: ", mainTitle.nodeType);
 console.log("the Inner HTML is ", mainTitle.innerHTML );
 console.log("Child nodes: ", mainTitle.childNodes.length);
+
+var myLinks = document.getElementsByTagName("a");
+console.log("Links: ", myLinks.length);
