752fb3293897767c17fa5b6eb6dbdf62aba5fa57
Lilanka Ravinath <lilankaravinath@outlook.com>
2017-06-14 10:54:03 +0530

    Get the current h,m & sec with Date() object

--- project_Creat_an_Analog_Clock/script.js
+++ project_Creat_an_Analog_Clock/script.js
@@ -6,6 +6,14 @@ let hrPosition = 20;
 let minPosition = 130;
 let secPosition = 256;
 
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+console.log("Hours: " + hr + " Minute: " + min + " Second: " + sec);
+
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
 SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
