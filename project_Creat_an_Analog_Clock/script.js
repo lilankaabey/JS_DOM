be5c2ab13ff4ec3fa030f00f4c121ebcce8cbab0
Lilanka Ravinath <lilankaravinath@outlook.com>
2017-06-16 05:46:10 +0530

    Show the current time using Math

--- project_Creat_an_Analog_Clock/script.js
+++ project_Creat_an_Analog_Clock/script.js
@@ -2,17 +2,18 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPosition = 20;
-let minPosition = 130;
-let secPosition = 256;
-
 var date = new Date();
+//view the date using console pannel
 console.log(date);
 let hr = date.getHours();
 let min = date.getMinutes();
 let sec = date.getSeconds();
-
+//view the time with hours, minute and second using console pannel
 console.log("Hours: " + hr + " Minute: " + min + " Second: " + sec);
+
+let hrPosition = (hr*360/12)+(min*(360/60)/12);
+let minPosition = (min*360/60)+(sec*(360/60)/60);
+let secPosition = sec*360/60;
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
