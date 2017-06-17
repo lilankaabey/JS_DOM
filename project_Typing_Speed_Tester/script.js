3a4791324fcbe6f612af274bf2cde98e40bff445
Lilanka Ravinath <lilankaravinath@outlook.com>
2017-06-17 23:26:46 +0530

    Stop the timer when the test is done

--- project_Typing_Speed_Tester/script.js
+++ project_Typing_Speed_Tester/script.js
@@ -5,7 +5,8 @@ const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
 var timer = [0,0,0,0];
-
+var interval;
+var timerRunning = false;
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function leadingZero(time) {
   if (time <= 9) {
@@ -30,6 +31,7 @@ function spellCheck(){
   let textEntered = testArea.value;
   let originTextMatch = originText.substring(0,textEntered.length);
   if (textEntered == originText) {
+    clearInterval(interval);
     testWrapper.style.borderColor = "#429890";
   }else {
     if(textEntered == originTextMatch) {
@@ -38,14 +40,14 @@ function spellCheck(){
       testWrapper.style.borderColor = "#E95D0F";
     }
   }
-  
 }
 
 // Start the timer:
 function start(){
   let textEnterdLength = testArea.value.length;
-  if(textEnterdLength === 0){
-    setInterval(runTimer, 10);
+  if(textEnterdLength === 0 && !timerRunning){
+    timerRunning = true;
+    interval = setInterval(runTimer, 10);
   }
   console.log(textEnterdLength);
 }
